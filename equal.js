0f849add88bc927b3575dbb6abe58d06be1794ac
Shakil Ahmed <shakil200409@gmail.com>
2024-10-29 20:56:30 +0600

    Learned about equal problems which are faced due to double equal and solved by tripple equal

---
+++ equal.js
@@ -0,0 +1,24 @@
+const x = 1;
+const y = true;
+
+if(x === y){
+    console.log('Values are equal');
+}
+else{
+    console.log('Values are not equal');
+}
+
+
+
+/* Experimental funny things which are weird on JS */
+
+// console.log(true + false);
+// console.log(12 / "6");
+// console.log("number" + 15 + 3);
+// console.log([1] > null);
+// console.log("foo" + + "bar");
+// console.log('true' == true);
+// console.log(false == 'false');
+// console.log(null == '');
+// console.log(!!"false" == !!"true");
+// console.log(['x'] == 'x');
